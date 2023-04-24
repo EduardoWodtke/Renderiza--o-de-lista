8e191f2ffc07b74892049dc642015f3e7d15c9f0
eduardorwodtkeifc@gmail.com <eduardorwodtkeifc@gmail.com>
2023-04-24 11:48:00 -0300

    1231

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  const cidades = [
+    'São Paulo',
+    'Rio de Janeiro',
+    'Belo Horizonte',
+    'Salvador',
+    'Fortaleza',
+    'Curitiba',
+    'Manaus',
+    'Recife',
+    'Porto Alegre',
+    'Brasília'
+]
+</script>
+
+<template>
+  <h1>lista de cidades</h1>
+  <ul>
+    <li v-for="(cidade, index) in cidades" :key="index">{{ cidade }}</li>
+  </ul>
+</template>
+
+<style scoped>
+</style>
